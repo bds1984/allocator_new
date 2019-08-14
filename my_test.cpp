59406e3dfd916c761c8489f17946f7d0bf5c24ea
bds1984 <52322872+bds1984@users.noreply.github.com>
2019-08-14 16:04:05 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -18,6 +18,16 @@ BOOST_AUTO_TEST_CASE(test_m_ctr_2)
 my_container<int, my_allocator<int, 0>> m_ctr_test_2(4,5);
 BOOST_CHECK(m_ctr_test_2[2]==5);
 }
+
+BOOST_AUTO_TEST_CASE(test_m_ctr_3)
+{
+std::map<int, double, std::less<int>, my_allocator<std::pair<const int, double>, 10>> m_map_test;
+  for (int i = 0; i < 10; i++)
+    {
+        m_map_test.insert(std::make_pair(i, fact(i)));
+    }
+  BOOST_CHECK( m_map_test[7]==5040);
+}
 }
 
 
