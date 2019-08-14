9544db4deae0fc6dbebf8be65d349dc83ac21807
bds1984 <52322872+bds1984@users.noreply.github.com>
2019-08-14 16:17:35 +0300

    Update my_test.cpp

--- my_test.cpp
+++ my_test.cpp
@@ -5,21 +5,24 @@
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(my_test)
-
-BOOST_AUTO_TEST_CASE(test_m_ctr)
+ BOOST_AUTO_TEST_CASE(test_m_ctr)
 {
 my_container<int, my_allocator<int, 0>> m_ctr_test(10);
 m_ctr_test.push_back(250);
 BOOST_CHECK(m_ctr_test[10]==250);
 }
-
-BOOST_AUTO_TEST_CASE(test_m_ctr_2)
+BOOST_AUTO_TEST_SUITE_END()
+  
+BOOST_AUTO_TEST_SUITE(my_test_1)
+ BOOST_AUTO_TEST_CASE(test_m_ctr_2)
 {
 my_container<int, my_allocator<int, 0>> m_ctr_test_2(4,5);
 BOOST_CHECK(m_ctr_test_2[2]==5);
 }
+BOOST_AUTO_TEST_SUITE_END()
 
-BOOST_AUTO_TEST_CASE(test_m_ctr_3)
+BOOST_AUTO_TEST_SUITE(my_test_2)
+ BOOST_AUTO_TEST_CASE(test_m_ctr_3)
 {
 std::map<int, double, std::less<int>, my_allocator<std::pair<const int, double>, 10>> m_map_test;
   for (int i = 0; i < 10; i++)
